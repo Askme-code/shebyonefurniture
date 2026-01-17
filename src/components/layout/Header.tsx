feccedcec9a06fc11b2a2f2b89e4d06e397cdbf0
Trials Tricks <trialstricks@gmail.com>
2026-01-17 20:39:05 +0000

    ## Error Type Console Error
    ## Error Message
    A tree hydrated but some a

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -7,10 +7,16 @@ import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet';
 import { Logo } from './Logo';
 import { useCart } from '@/hooks/use-cart';
 import { categories } from '@/lib/data';
+import { useEffect, useState } from 'react';
 
 export function Header() {
   const { items } = useCart();
   const cartItemCount = items.reduce((acc, item) => acc + item.quantity, 0);
+
+  const [mounted, setMounted] = useState(false);
+  useEffect(() => {
+    setMounted(true);
+  }, []);
 
   const navLinks = [
     { href: '/', label: 'Home' },
@@ -61,23 +67,31 @@ export function Header() {
                 </div>
               </Button>
             </Link>
-            <Sheet>
-              <SheetTrigger asChild>
+            {mounted ? (
+                <Sheet>
+                  <SheetTrigger asChild>
+                    <Button variant="ghost" size="icon" className="md:hidden">
+                      <Menu className="h-6 w-6" />
+                      <span className="sr-only">Toggle navigation menu</span>
+                    </Button>
+                  </SheetTrigger>
+                  <SheetContent side="left">
+                    <nav className="grid gap-6 text-lg font-medium mt-8">
+                      {navLinks.map(link => (
+                         <Link key={link.href} href={link.href} className="text-muted-foreground transition-colors hover:text-foreground">
+                            {link.label}
+                         </Link>
+                      ))}
+                    </nav>
+                  </SheetContent>
+                </Sheet>
+              ) : (
                 <Button variant="ghost" size="icon" className="md:hidden">
-                  <Menu className="h-6 w-6" />
-                  <span className="sr-only">Toggle navigation menu</span>
+                    <Menu className="h-6 w-6" />
+                    <span className="sr-only">Toggle navigation menu</span>
                 </Button>
-              </SheetTrigger>
-              <SheetContent side="left">
-                <nav className="grid gap-6 text-lg font-medium mt-8">
-                  {navLinks.map(link => (
-                     <Link key={link.href} href={link.href} className="text-muted-foreground transition-colors hover:text-foreground">
-                        {link.label}
-                     </Link>
-                  ))}
-                </nav>
-              </SheetContent>
-            </Sheet>
+              )
+            }
           </div>
         </div>
       </div>
