dfbcf0b295cce290aa1ad0a6a1e4348b4379ee48
Trials Tricks <trialstricks@gmail.com>
2026-01-17 21:59:41 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6100498251-98b49",
+  "appId": "1:270545203542:web:faaed55532380f057fa6f7",
+  "apiKey": "AIzaSyDN2MpbRrGh2Yf_GDdBOlv1rxK3nKFjV_s",
+  "authDomain": "studio-6100498251-98b49.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "270545203542"
+};
